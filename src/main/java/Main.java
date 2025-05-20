78d9e4ae42cf96e0f2f0582bcaebdfb206c7c4c4
MaciasMateusz173491 <173491@stud.prz.edu.pl>
2025-05-20 13:10:07 +0000

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,8 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
-//
-import java.util.Scanner;
-
+// napisz program
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int count = 0;
-
-        while (count < 20) {
-            System.out.println("Naciśnij Enter, aby kontynuować...");
-            scanner.nextLine(); // Czekaj na naciśnięcie Enter
-            count++;
-            System.out.println(count);
-        }
-
-        System.out.println("Koniec liczenia.");
-        scanner.close();
+  public static void main(String[] args) {
+    for (int i = 1; i <= 20; i++) {
+      System.out.println(i);
     }
+  }
 }
